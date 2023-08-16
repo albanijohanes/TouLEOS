f6f87a16c5edba57cd8d68ccbef2f61925beef10
Roma Nouke Frets Mantiri <romamantiri17@gmail.com>
2023-08-16 16:51:10 +0800

    edit fillable line 24 delete alamat from db

--- app/User.php
+++ app/User.php
@@ -16,7 +16,13 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'nama',
+        'role',
+        'email',
+        'password',
+        'no_hp',
+        'jk',
+        'username',
     ];
 
     /**
