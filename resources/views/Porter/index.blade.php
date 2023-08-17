e87997a8952f5e679ed304b2619163036c9ea3d6
Roma Nouke Frets Mantiri <romamantiri17@gmail.com>
2023-08-17 10:16:41 +0800

    edit footer porter in index and modify users profile

--- resources/views/Porter/index.blade.php
+++ resources/views/Porter/index.blade.php
@@ -32,7 +32,6 @@
     <link href="{{ asset('porterassets/vendor/simple-datatables/style.css') }}"
         rel="stylesheet">
 
-    <!-- Template Main CSS File -->
     <link href="{{ asset('porterassets/css/style.css') }}" rel="stylesheet">
 
 </head>
@@ -362,13 +361,6 @@
         <div class="copyright">
             &copy; Copyright <strong><span>TouLEOS</span></strong>. All Rights Reserved
         </div>
-        <div class="credits">
-            <!-- All the links in the footer should remain intact. -->
-            <!-- You can delete the links only if you purchased the pro version. -->
-            <!-- Licensing information: https://bootstrapmade.com/license/ -->
-            <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/nice-admin-bootstrap-admin-html-template/ -->
-            Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
-        </div>
     </footer><!-- End Footer -->
 
     <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i
@@ -386,7 +378,6 @@
     <script src="{{ asset('porterassets/vendor/tinymce/tinymce.min.js') }}"></script>
     <script src="{{ asset('porterassets/vendor/php-email-form/validate.js') }}"></script>
 
-    <!-- Template Main JS File -->
     <script src="{{ asset('porterassets/js/main.js') }}"></script>
 
 </body>
--- resources/views/Porter/users-profile.blade.php
+++ resources/views/Porter/users-profile.blade.php
@@ -375,13 +375,6 @@
         <div class="copyright">
             &copy; Copyright <strong><span>TouLEOS</span></strong>. All Rights Reserved
         </div>
-        <div class="credits">
-            <!-- All the links in the footer should remain intact. -->
-            <!-- You can delete the links only if you purchased the pro version. -->
-            <!-- Licensing information: https://bootstrapmade.com/license/ -->
-            <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/nice-admin-bootstrap-admin-html-template/ -->
-            Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
-        </div>
     </footer><!-- End Footer -->
 
     <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i
@@ -397,7 +390,6 @@
     <script src="{{ asset('porter/vendor/tinymce/tinymce.min.js') }}"></script>
     <script src="{{ asset('porter/vendor/php-email-form/validate.js') }}"></script>
 
-    <!-- Template Main JS File -->
     <script src="{{ asset('porter/js/main.js') }}"></script>
 
 </body>
