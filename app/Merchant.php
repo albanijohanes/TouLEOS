624ae4b62d3619f583add3983a742cd1264b9916
albanijohanes <albanijohanes106@student.unsrat.ac.id>
2023-08-22 16:46:47 +0800

    update app/Merchant.php
    Adding line 9 - 18:
    - line 9, protected $fillable for database Merchant
    - line 15, function User for model User

--- app/Merchant.php
+++ app/Merchant.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Merchant extends Model
 {
-    //
+    protected $fillable = [
+        'user_id',
+        'ktp',
+        ''
+    ];
+
+    public function User(){
+        return $this->belongsTo(User::class);
+    }
 }
