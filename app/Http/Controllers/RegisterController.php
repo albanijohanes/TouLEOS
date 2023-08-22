e0308995dbdc01459e792b8a253a42c243011e59
albanijohanes <albanijohanes106@student.unsrat.ac.id>
2023-08-22 20:02:25 +0800

    update App/Http/Controllers/RegisterController.php
    Adding line 5 - 68:
    - line 5, use App/Merchant;
    - line 6, use App/Porter;
    - line 10, use Illuminate/Support/Facedes/Validator as FacedesValidator;
    - line 14, function Register update:
    	- Update Request $request,
    	- Update query for role,
    	- update compact for role
    - line 19, function RegisterPost update:
    	- Update Validator for rule register,
    	- Update User::create,
    	- Adding if loop for request role porter and merchant,
    - line 66, New session if success

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -2,34 +2,68 @@
 
 namespace App\Http\Controllers;
 
+use App\Merchant;
+use App\Porter;
 use App\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator as FacadesValidator;
 
 class RegisterController extends Controller
 {
-    public function register(){
-        return view('auth/register');
+    public function register(Request $request){
+        $role = $request->query('role');
+        return view('auth/register', compact('role'));
     }
 
     public function registerPost(Request $request){
-        $user           = User::create([
-            'nama'      => $request->nama,
-            'username'  => $request->username,
-            'no_hp'     => $request->no_hp,
-            'jk'        => $request->jk,
-            'alamat'    => $request->alamat,
-            'role'      => $request->role,
-            'password'  => Hash::make($request->password),
+
+        $validator = FacadesValidator::make($request->all(), [
+            'nama' => 'required|string|max: 255',
+            'username' => 'required|string|max:255|unique:users',
+            'no_hp' => 'required|string|max:255',
+            'jk' => 'required|string|max:255',
+            'role' => 'required|string|max:255',
+            'password' => 'required|string|min:8|confirmed',
+            'pdf1' => 'required_if:role,porter,merchant|file|mimes:pdf',
+            'pdf2' => 'required_if:role,porter,merchant|file|mimes:pdf',
+        ]);
+
+        if ($validator->fails()) {
+            return redirect()->back()->withErrors($validator)->withInput();
+        }
+
+        $user = User::create([
+            'nama' => $request->nama,
+            'username' => $request->username,
+            'no_hp' => $request->no_hp,
+            'role' => $request->role,
+            'password' => Hash::make($request->password),
         ]);
 
         if($user){
-            session()->flash('success', 'Registerasi berhasil');
-            return redirect()->route('login');
-        }else{
-            session()->flash('error', 'Tolong di isi dengan benar');
-            return back();
+            if ($request->role === 'porter' || $request->role === 'merchant'){
+                $skkb = $request->skkb->store('user_pdfs');
+                $ktp = $request->ktp->store('user_pdfs');
+                $siup = $request->siup->store('user_pdfs');
+
+                if($request->role === 'porter'){
+                    Porter::create([
+                        'user_id' => $request->user->id,
+                        'skkb' => $skkb,
+                        'ktp' => $ktp
+                    ]);
+                } elseif ($request->role === 'merchant') {
+                    Merchant::create([
+                        'user_id' => $request->user->id,
+                        'siup' => $siup,
+                        'ktp' => $ktp
+                    ]);
+                }
+            }
         }
 
+        session()->flash('success', 'Register Berhasil');
+        return redirect()->route('login');
     }
 }
