230ff7e6ba3f84f9fa9240ef51e0b216e85886c8
albanijohanes <albanijohanes106@student.unsrat.ac.id>
2023-08-22 16:44:05 +0800

    update app/Porter.php
    Adding line 9 - 18:
    - line 9, add protected $fillable for database Porter
    - line 15, function user for model user

--- app/Porter.php
+++ app/Porter.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Porter extends Model
 {
-    //
+    protected $fillable = [
+        'user_id',
+        'skkb',
+        'ktp'
+    ];
+
+    public function User(){
+        return $this->belongsTo(User::class);
+    }
 }
