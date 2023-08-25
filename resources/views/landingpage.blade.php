734f2c8c0591c8a504617582fa840f858a8453d3
albanijohanes <albanijohanes106@student.unsrat.ac.id>
2023-08-25 21:56:51 +0800

    Add landing page:
    update UI for landing page

--- resources/views/landingpage.blade.php
+++ resources/views/landingpage.blade.php
@@ -5,21 +5,32 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
     <title>TouLEOS</title>
-    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="{{ asset('landingassets/bootstrap/css/bootstrap.min.css') }}">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Aclonica&amp;display=swap">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Alfa+Slab+One&amp;display=swap">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poppins&amp;display=swap">
-    <link rel="stylesheet" href="assets/css/bs-theme-overrides.css">
-    <link rel="stylesheet" href="assets/css/Service-Box-Style-01.css">
+    <link rel="stylesheet" href="{{ asset('landingassets/css/bs-theme-overrides.css') }}">
+    <link rel="stylesheet" href="{{ asset('landingassets/css/Service-Box-Style-01.css') }}">
 </head>
 
-<body style="padding-top: 0px;background: linear-gradient(rgba(105,72,45,0.9), rgba(109,205,61,0.9)), url(&quot;assets/img/20230712_124645.jpg&quot;) center / cover repeat, rgb(164,125,125);"><img src="assets/img/Logo%20web%20Tou%20Leos%20(3).png" style="width: 150px;padding-bottom: 0px;padding-top: 26px;margin-top: -24px;padding-left: 0px;margin-left: 24px;"><img src="assets/img/Logo%20web%20Tou%20Leos%20(4).png" style="width: 180px;padding-bottom: 0px;padding-left: 31px;margin-left: -32px;">
-    <h1 style="text-align: center;color: #ffffff;font-family: Poppins, sans-serif;font-size: 63px;padding-top: 56px;margin-top: -52px;">Selamat datang !</h1>
+<body
+    style="padding-top: 0px;background: linear-gradient(rgba(105,72,45,0.9), rgba(109,205,61,0.9)), url(&quot;landingassets/img/20230712_124645.jpg&quot;) center / cover repeat, rgb(164,125,125);">
+    <img src="{{ asset('landingassets/img/Logo%20web%20Tou%20Leos%20(3).png') }}"
+        style="width: 150px;padding-bottom: 0px;padding-top: 26px;margin-top: -24px;padding-left: 0px;margin-left: 24px;"><img
+        src="{{ asset('landingassets/img/Logo%20web%20Tou%20Leos%20(4).png') }}"
+        style="width: 180px;padding-bottom: 0px;padding-left: 31px;margin-left: -32px;">
+    <h1
+        style="text-align: center;color: #ffffff;font-family: Poppins, sans-serif;font-size: 63px;padding-top: 56px;margin-top: -52px;">
+        Selamat datang !</h1>
     <div class="container" style="padding-top: 0px;margin-top: -116px;margin-bottom: 110px;padding-left: 0px;">
-        <div class="row" style="margin-left: 68px;margin-right: 70px;margin-bottom: 142px;padding-top: 0px;margin-top: 132px;">
+        <div class="row"
+            style="margin-left: 68px;margin-right: 70px;margin-bottom: 142px;padding-top: 0px;margin-top: 132px;">
             <div class="col-sm-4 col-md-4">
                 <div class="serviceBox yellow">
-                    <div class="text-center service-icon"><a href="#"><svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icon-tabler-walk">
+                    <div class="text-center service-icon"><a href="#"><svg xmlns="http://www.w3.org/2000/svg"
+                                width="1em" height="1em" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor"
+                                fill="none" stroke-linecap="round" stroke-linejoin="round"
+                                class="icon icon-tabler icon-tabler-walk">
                                 <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
                                 <circle cx="13" cy="4" r="1"></circle>
                                 <line x1="7" y1="21" x2="10" y2="17"></line>
@@ -27,14 +38,21 @@
                                 <path d="M6 12l2 -3l4 -1l3 3l3 1"></path>
                             </svg></a></div>
                     <div class="service-content" style="padding-top: 16px;font-family: Poppins, sans-serif;">
-                        <h3 style="margin-top: 20px;margin-right: -53px;margin-left: -49px;padding-left: 20px;padding-right: 24px;" class="pengunjung">pengunjung pasar</h3>
+                        <h3 style="margin-top: 20px;margin-right: -53px;margin-left: -49px;padding-left: 20px;padding-right: 24px;"
+                            class="pengunjung">pengunjung pasar</h3>
                         <hr>
-                    </div><button class="btn btn-primary" type="button" style="padding-left: 20px;padding-right: 20px;margin-right: 20px;margin-left: 20px;margin-bottom: 5px;font-family: Poppins, sans-serif;font-size: 14px;">MASUK</button><button class="btn btn-primary" type="button" style="padding-left: 20px;padding-right: 20px;margin-right: 20px;margin-bottom: 5px;margin-left: 20px;font-family: Poppins, sans-serif;font-size: 14px;">DAFTAR</button>
+                    </div><button class="btn btn-primary" type="button"
+                        style="padding-left: 20px;padding-right: 20px;margin-right: 20px;margin-left: 20px;margin-bottom: 5px;font-family: Poppins, sans-serif;font-size: 14px;">MASUK</button><button
+                        class="btn btn-primary" type="button"
+                        style="padding-left: 20px;padding-right: 20px;margin-right: 20px;margin-bottom: 5px;margin-left: 20px;font-family: Poppins, sans-serif;font-size: 14px;">DAFTAR</button>
                 </div>
             </div>
             <div class="col-sm-4 col-md-4">
                 <div class="serviceBox yellow">
-                    <div class="text-center service-icon"><a href="#"><svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icon-tabler-users">
+                    <div class="text-center service-icon"><a href="#"><svg xmlns="http://www.w3.org/2000/svg"
+                                width="1em" height="1em" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor"
+                                fill="none" stroke-linecap="round" stroke-linejoin="round"
+                                class="icon icon-tabler icon-tabler-users">
                                 <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
                                 <circle cx="9" cy="7" r="4"></circle>
                                 <path d="M3 21v-2a4 4 0 0 1 4 -4h4a4 4 0 0 1 4 4v2"></path>
@@ -44,25 +62,36 @@
                     <div class="service-content" style="padding-top: 16px;">
                         <h3 style="margin-top: 20px;font-family: Poppins, sans-serif;" class="porter">porter</h3>
                         <hr>
-                    </div><button class="btn btn-primary" type="button" style="padding-left: 20px;padding-right: 20px;margin-left: 20px;margin-right: 20px;font-family: Poppins, sans-serif;">MASUK</button><button class="btn btn-primary" type="button" style="padding-left: 20px;padding-right: 20px;margin-left: 20px;margin-right: 20px;margin-bottom: 5px;font-family: Poppins, sans-serif;font-size: 14px;">DAFTAR</button>
+                    </div><button class="btn btn-primary" type="button"
+                        style="padding-left: 20px;padding-right: 20px;margin-left: 20px;margin-right: 20px;font-family: Poppins, sans-serif;">MASUK</button><button
+                        class="btn btn-primary" type="button"
+                        style="padding-left: 20px;padding-right: 20px;margin-left: 20px;margin-right: 20px;margin-bottom: 5px;font-family: Poppins, sans-serif;font-size: 14px;">DAFTAR</button>
                 </div>
             </div>
             <div class="col-sm-4 col-md-4">
                 <div class="serviceBox yellow">
-                    <div class="text-center service-icon"><a href="#"><svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icon-tabler-smart-home">
+                    <div class="text-center service-icon"><a href="#"><svg xmlns="http://www.w3.org/2000/svg"
+                                width="1em" height="1em" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor"
+                                fill="none" stroke-linecap="round" stroke-linejoin="round"
+                                class="icon icon-tabler icon-tabler-smart-home">
                                 <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-                                <path d="M19 8.71l-5.333 -4.148a2.666 2.666 0 0 0 -3.274 0l-5.334 4.148a2.665 2.665 0 0 0 -1.029 2.105v7.2a2 2 0 0 0 2 2h12a2 2 0 0 0 2 -2v-7.2c0 -.823 -.38 -1.6 -1.03 -2.105"></path>
+                                <path
+                                    d="M19 8.71l-5.333 -4.148a2.666 2.666 0 0 0 -3.274 0l-5.334 4.148a2.665 2.665 0 0 0 -1.029 2.105v7.2a2 2 0 0 0 2 2h12a2 2 0 0 0 2 -2v-7.2c0 -.823 -.38 -1.6 -1.03 -2.105">
+                                </path>
                                 <path d="M16 15c-2.21 1.333-5.792 1.333-8 0"></path>
                             </svg></a></div>
                     <div class="service-content" style="padding-top: 16px;font-family: Poppins, sans-serif;">
                         <h3 style="margin-top: 20px;" class="merchant">merchant</h3>
                         <hr>
-                    </div><button class="btn btn-primary" type="button" style="padding-left: 20px;padding-right: 20px;margin-left: 20px;margin-right: 20px;font-family: Poppins, sans-serif;">MASUK</button><button class="btn btn-primary" type="button" style="padding-left: 20px;padding-right: 20px;margin-left: 20px;margin-right: 20px;margin-bottom: 5px;font-family: Poppins, sans-serif;">DAFTAR</button>
+                    </div><button class="btn btn-primary" type="button"
+                        style="padding-left: 20px;padding-right: 20px;margin-left: 20px;margin-right: 20px;font-family: Poppins, sans-serif;">MASUK</button><button
+                        class="btn btn-primary" type="button"
+                        style="padding-left: 20px;padding-right: 20px;margin-left: 20px;margin-right: 20px;margin-bottom: 5px;font-family: Poppins, sans-serif;">DAFTAR</button>
                 </div>
             </div>
         </div>
     </div>
-    <script src="assets/bootstrap/js/bootstrap.min.js"></script>
+    <script src="{{ asset('landingassets/bootstrap/js/bootstrap.min.js') }}"></script>
 </body>
 
 </html>
