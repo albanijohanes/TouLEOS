e2076f921f5186d8b18b4a685939a7931e658e74
albanijohanes <albanijohanes106@student.unsrat.ac.id>
2023-08-26 09:11:06 +0800

    update seed for database seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,9 @@
 <?php
 
+use App\role;
+use App\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -11,6 +14,31 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UserSeeder::class);
+        // Create roles
+        $customerRole = role::create(['nama' => 'Customer']);
+        $porterRole = role::create(['nama' => 'Porter']);
+        $merchantRole = role::create(['nama' => 'Merchant']);
+
+        // Create users
+        $customer = User::create([
+            'nama' => 'John Doe',
+            'email' => 'john@example.com',
+            'password' => Hash::make('password'),
+            'role_id' => $customerRole->id,
+        ]);
+
+        $porter = User::create([
+            'nama' => 'Jane Porter',
+            'email' => 'jane@example.com',
+            'password' => Hash::make('password'),
+            'role_id' => $porterRole->id,
+        ]);
+
+        $merchant = User::create([
+            'nama' => 'Mike Merchant',
+            'email' => 'mike@example.com',
+            'password' => Hash::make('password'),
+            'role_id' => $merchantRole->id,
+        ]);
     }
 }
