f42293ca3dddc78dae679a9c6ca87aef85d7685a
albanijohanes <albanijohanes106@student.unsrat.ac.id>
2023-09-04 17:19:32 +0800

    Create App/Http/Controllers/CustomerController.php
    - Create controller customer for customer page every request and page view

---
+++ app/Http/Controllers/CustomerController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\User;
+use Illuminate\Http\Request;
+
+class CustomerController extends Controller
+{
+    public function index(){
+        return view('user/index');
+    }
+    public function edituser(Request $request, $id){
+        $user = User::find($id);
+        $user->update($request->all());
+        return redirect()->route('userporter')->with('success', 'Informasi anda telah di update');
+    }
+    public function profiluser(){
+        return view('user/profil_user');
+    }
+    public function services(){
+        return view('user/services');
+    }
+}
